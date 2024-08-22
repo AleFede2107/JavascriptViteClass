25b32eab78a697f1e2caa40e4986563581a4913b
Federico <alegriafederico0@gmail.com>
2024-08-21 19:36:02 -0600

    :sparkles: feat: light and dark mode

--- main.js
+++ main.js
@@ -7,3 +7,26 @@ document.querySelector('#app').innerHTML += Title();
 document.querySelector('#app').innerHTML += Subtitle('Federico');
 document.querySelector('#app').innerHTML += Subtitle('Fernando');
 document.querySelector('#app').innerHTML += Menu();
+document.querySelector('#app').innerHTML += `
+<section>
+  <h2>Automatic theme</h2>
+  <p>This is a Section</p>
+</section>
+
+<section class="light">
+  <h2>Light theme</h2>
+  <p>This is a Section</p>
+</section>
+
+<section class="dark">
+  <h2>Dark theme</h2>
+  <p>This is a Section</p>
+</section>
+
+<button type="button" id="theme-toggle">Change Theme</button>
+`
+const themeToggle = document.getElementById('theme-toggle');
+
+themeToggle.addEventListener('click', () => {
+  document.documentElement.classList.toggle('light')
+});
