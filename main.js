057152864253e48b223d54df1a39d091b41345af
Federico <alegriafederico0@gmail.com>
2024-08-21 20:10:25 -0600

    :sparkles: feat:

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './style.css'
 import {Title, Menu} from './components/Title.js'
 import Subtitle from'./components/Subtitle.js'
 
+const tasks = [];
+
 document.querySelector('#app').innerHTML = Title();
 document.querySelector('#app').innerHTML += Title();
 document.querySelector('#app').innerHTML += Subtitle('Federico');
@@ -24,7 +26,33 @@ document.querySelector('#app').innerHTML += `
 </section>
 
 <button type="button" id="theme-toggle">Change Theme</button>
+
+<h2>Task Manager</h2>
+<form id="task-form">
+  <input type="text" placeholder="Write a task" id="taskName"/>
+  <textarea placeholder="Description" id="taskDescription"></textarea>
+  <button type="submit">Add Task</button>
+</form>
+
+<ul id="task-list">
+</ul>
 `
+const addTask = (event) =>{
+  event.preventDefault()
+  const taskInput = document.querySelector('#taskName').value
+  const taskDescription = document.querySelector('#taskDescription').value
+
+  const newTask = {
+    title: taskInput,
+    description: taskDescription
+  }
+
+  tasks.push(newTask)
+  console.log(tasks)
+}
+
+document.querySelector('#task-form').addEventListener('submit', addTask);
+
 const themeToggle = document.getElementById('theme-toggle');
 
 themeToggle.addEventListener('click', () => {
