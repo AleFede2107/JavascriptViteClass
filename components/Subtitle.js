b36a74057c5f47e76769a4602faf8084ff9ad011
Federico <alegriafederico0@gmail.com>
2024-08-21 19:03:38 -0600

    :sparkles: feat: javascript components

---
+++ components/Subtitle.js
@@ -0,0 +1,5 @@
+ const Subtitle = (name) => {
+    return `<h2>Esta app es creada por ${name}</h2>`
+  }
+
+  export default Subtitle;
---
+++ components/Title.js
@@ -0,0 +1,9 @@
+export const Title = () => {
+    return `<h1>Mi primera app con JS + Vite.js</h1>`
+  }
+
+export const Menu = () => {
+    return `
+        <p>Esto es un Menu</p>
+    `
+}
